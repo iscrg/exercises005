e874e610ee36b4e20b028e100b4e3149e1bc293f
Иван <ispjxfer@mail.ru>
2024-02-12 23:39:08 +0700

    fix

--- 8.py
+++ 8.py
@@ -2,7 +2,7 @@ nums = set(map(str, input().split()))
 res = []
 
 for i in range(len(nums)):
-    for j in range(i + 1, len(nums)):
+    for j in range(i, len(nums)):
         res_set = {list(nums)[k] for k in range(i, j+1)}
         res.append(res_set)
 
